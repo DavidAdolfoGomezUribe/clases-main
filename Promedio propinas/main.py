e40f3ae55146861c68964efd0c4e808b6c22e7cf
DavidAdolfoGomezUribe <96323893+DavidAdolfoGomezUribe@users.noreply.github.com>
2024-11-08 13:02:29 -0500

    Clase 08/11/2024

---
+++ Promedio propinas/main.py
@@ -0,0 +1,5 @@
+from menu.mainMenu import desing
+from menu.calculateTipMenu import desing as desingOption1
+from menu.divideAmointsMenu import desing as desingOption2
+
+respuesta = desingOption2()
---
+++ Promedio propinas/menu/divideAmointsMenu.py
@@ -0,0 +1,27 @@
+import os
+from formula.logic import   calcular_propina, calcular_total_con_propina,dividir_total_personas
+def desing():
+    while True:
+        print(f"""
+        =============================================
+            Dividir Cuenta entre Varias Personas
+        =============================================""")
+        total = float(input("Ingrese el monto total de la cuenta: $____"))
+        porcentaje = int(input("Ingrese el porcentaje de propina (por ejemplo: 15): ___ %"))
+        persona =  int(input("Ingrese el número de personas: __"))
+
+        propina = calcular_propina(total, porcentaje)
+        totalMasPropina = calcular_total_con_propina(total,propina)
+        print(f"""
+        =============================================
+        La propina calculada es: ${propina}
+        El total a pagar es: $___{totalMasPropina}
+        Monto por persona: $___{dividir_total_personas(total,persona)}
+        =============================================
+
+    """)
+        options = str(input("¿Deseas calcular nuevamente? (S/N)"))
+        if options == "S" or options == "s" :
+            os.system("clear")
+        else:
+            break    
