fff94cf646dabffbc8d276809d71c5be2c764708
DavidAdolfoGomezUribe <96323893+DavidAdolfoGomezUribe@users.noreply.github.com>
2024-11-12 10:35:19 -0500

    test 12/11/2024

--- Promedio propinas/main.py
+++ Promedio propinas/main.py
@@ -5,13 +5,14 @@ from menu.mainMenu import desing
 from menu.calculateTipMenu import desing as desingOption1
 from menu.divideAmointsMenu import desing as desingOption2
 
-option = desingOption1()
-if option == 1:
-    desing()
-elif option == 2:
-    desingOption2()    
-elif option == 3:
-    print("        Gracias por usar el programa")
-    sys.exit()
-else :
-    sys.exit()
+while True:
+    option = desingOption1()
+    if option == 1:
+        desing()
+    elif option == 2:
+        desingOption2()    
+    elif option == 3:
+        print("        Gracias por usar el programa")
+        sys.exit()
+    else :
+        sys.exit()
